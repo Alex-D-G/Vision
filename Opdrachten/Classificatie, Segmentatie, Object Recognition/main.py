05a922475ee0b4ec4c533cf111af142357dbac1e
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2021-03-20 12:49:00 +0100

    Update main.py

--- Opdrachten/Classificatie, Segmentatie, Object Recognition/main.py
+++ Opdrachten/Classificatie, Segmentatie, Object Recognition/main.py
@@ -1,21 +1,24 @@
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn import svm
+from sklearn import datasets, svm, utils
 import random
 
 digits = datasets.load_digits()
+
+digits.target, digits.data = utils.shuffle(digits.target, digits.data)
+
 two_third = int(len(digits.data)/3*2)  #Selects only 2/3 of the data
 
 clf = svm.SVC(gamma=0.001, C=100)
 X, y = digits.data[:-two_third], digits.target[:-two_third]
 clf.fit(X, y)
 
-random.shuffle(digits.data)
-
-for x in range(30):
+amount_wrong = 0
+for x in range(len(digits.data)-two_third):
     target = -random.randint(1, len(digits.data)-two_third)
-    print("predict:", clf.predict(digits.data[target:target+1]))
-    plt.imshow(digits.images[target], cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.show()
 
-"Van de 30 runs was er 1 fout, dus een accuracy van 96.67%"
+    predict = clf.predict(digits.data[target:target+1])
+    answer = digits.target[target:target+1]
+    if predict != answer:
+        amount_wrong += 1
+
+print(amount_wrong, "out of", len(digits.data)-two_third, "wrong")
+print("Percentage correct:", 100-round((amount_wrong / (len(digits.data)-two_third)) * 100), "%")
