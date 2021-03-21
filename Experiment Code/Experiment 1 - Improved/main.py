69a598e7266fd2ef27f642ee9bcc115badecfa57
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2021-03-21 13:51:20 +0100

    Update main.py

--- Experiment Code/Experiment 1 - Improved/main.py
+++ Experiment Code/Experiment 1 - Improved/main.py
@@ -1,17 +1,11 @@
-from skimage.viewer import ImageViewer
 from skimage.transform import resize
 from skimage import io
 import numpy as np
 
 
-def saveAndShowImage(image, name):
-    viewer = ImageViewer(image)
-    viewer.show()
-    io.imsave(name+".jpg", image)
-
-
 def SimplifyEggs(image):
-    image = resize(image, (image.shape[0] // 4, image.shape[1] // 4), anti_aliasing=True)
+    size_multiplier = 4
+    image = resize(image, (image.shape[0] // size_multiplier, image.shape[1] // size_multiplier), anti_aliasing=True)
 
     for row in range(len(image)):
         for pixel in range(len(image[row])):
@@ -25,21 +19,21 @@ def SimplifyEggs(image):
 
             red_percentage = (image[row][pixel][0] / (int(image[row][pixel][1]) + int(image[row][pixel][2]))) * 100
 
-            if red_percentage < 140:
+            if red_percentage < 110:
                 image[row][pixel] = [0, 255, 255]
             else:
                 image[row][pixel] = [0, 0, 0]
 
-    image = resize(image, (image.shape[0] * 4, image.shape[1] * 4), anti_aliasing=True)
+    image = resize(image, (image.shape[0] * size_multiplier, image.shape[1] * size_multiplier), anti_aliasing=True)
     image = image.astype(np.uint8)
     return image
 
 
-egg1 = io.imread('egg1.jpg')
-egg2 = io.imread('egg2.jpg')
-egg3 = io.imread('egg3.jpg')
+def convert_eggs(location):
+    egg_images = io.imread_collection(location)
 
-saveAndShowImage( SimplifyEggs(egg1), "egg1_Result" )
-saveAndShowImage( SimplifyEggs(egg2), "egg2_Result" )
-saveAndShowImage( SimplifyEggs(egg3), "egg3_Result" )
+    for i, egg in enumerate(egg_images):
+        io.imsave("result_"+ str(i) + ".jpg", SimplifyEggs(egg))
 
+
+convert_eggs('to_convert/*.jpg')
